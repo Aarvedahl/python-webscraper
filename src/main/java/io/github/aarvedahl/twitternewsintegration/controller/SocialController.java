f74939f90d4d75be35a666ad52cbae3e2e6d59c1
Alex Arvedahl <alex.arvedahl@gmail.com>
2018-04-04 19:54:12 +0200

    Fixing git ingore

--- src/main/java/io/github/aarvedahl/twitternewsintegration/controller/SocialController.java
+++ src/main/java/io/github/aarvedahl/twitternewsintegration/controller/SocialController.java
@@ -4,7 +4,7 @@ import io.github.aarvedahl.twitternewsintegration.dto.Post;
 import io.github.aarvedahl.twitternewsintegration.dto.User;
 
 public abstract class SocialController {
-    
+
     public abstract User findUser(String name);
 
     public abstract Post findPosts(String hashtag);
