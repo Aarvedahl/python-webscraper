c6ec3727d59318bab8db04ba5a8c4c7d58e1e906
Alex Arvedahl <alex.arvedahl@gmail.com>
2018-04-05 17:23:02 +0200

    Began with twitter controller tests

---
+++ src/test/java/io/github/aarvedahl/twitternewsintegration/TwitterControllerTest.java
@@ -0,0 +1,28 @@
+package io.github.aarvedahl.twitternewsintegration;
+
+import io.github.aarvedahl.twitternewsintegration.controller.TwitterController;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TwitterControllerTest {
+
+    TwitterController twitterController;
+
+    @Before
+    public void init() {
+        twitterController = new TwitterController();
+    }
+
+
+    @Test
+    public void findUser() {
+
+    }
+
+
+
+
+
+}
