1800ed2e5d6523af9f038c5aceed8191f73df55f
Alex Arvedahl <alex.arvedahl@gmail.com>
2018-04-14 16:13:31 +0200

    Remaking twittercontroller

--- src/main/java/io/github/aarvedahl/twitternewsintegration/controller/TwitterController.java
+++ src/main/java/io/github/aarvedahl/twitternewsintegration/controller/TwitterController.java
@@ -1,16 +1,35 @@
 package io.github.aarvedahl.twitternewsintegration.controller;
 
-
+import com.google.gson.Gson;
 import io.github.aarvedahl.twitternewsintegration.dto.Status;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.util.List;
+
 public class TwitterController {
-    RestTemplate template;
+    OkHttpClient client;
+    Gson gson = new Gson();
 
     public Status searchTweets(String searchTweets) {
         // Set the correct header
-        template  = new RestTemplate();
-        Status status = template.getForObject("https://api.twitter.com/1.1/search/tweets.json?q=nasa&result_type=popular", Status.class);
+
+        Status status = gson.fromJson(run(url), Status.class);
         return status;
     }
+
+    public String run(String url, List<String> header) throws IOException {
+        client= new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
 }
